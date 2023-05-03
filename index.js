84d89741accd5a081432549921a494f2d74fe14e
arslonga83 <jlwood83@gmail.com>
2023-05-02 20:01:41 -0700

    submit form data with fetch request

--- index.js
+++ index.js
@@ -2,10 +2,20 @@ const contactForm = document.getElementById('contact-form')
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault()
+  console.log('click')
   const formData = new FormData(contactForm)
   const entries = formData.entries()
   const data = Object.fromEntries(entries)
-  console.log(data)
+  console.log(JSON.stringify(data))
 
-  contactForm.reset()
+  fetch('http://127.0.0.1:3000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 })
